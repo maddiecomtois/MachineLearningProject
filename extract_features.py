a089dfaf2ef745fca2c3dbca9f1ad96fd2f92d2f
Ciara Gilsenan <ciaragil98@gmail.com>
2020-11-25 13:07:13 +0000

    adding conversation type to feature matrix

--- extract_features.py
+++ extract_features.py
@@ -12,16 +12,18 @@ import numpy as np
 directory = './Datasets'
 familiar_tags = ['du', 'Du', 'dich', 'ihr', 'dir']
 formal_tags = ['Sie', 'Ihnen', 'Herr', 'Frau']
+convo_type_tags = ["customer service", "interview", "with friends", "hotel reservation", "hotel check in",
+                   "conversation with officer", "ordering at restaurant", "short conversation", "test cat"]
 convo_dataset = []
-
 
 # loop through all the convo files in the Datasets directory
 for filename in sorted(os.listdir(directory)):
     familiar_count = 0.0
     formal_count = 0.0
     entry_label = 0
+    dataset_entry = []
     # file_object = open(directory + '/' + filename, "r")
-    with open(directory + '/' + filename) as f:
+    with open(directory + '/' + filename, 'r') as f:
         # segment the contents into individual words
         contents = nltk.word_tokenize(f.read())
 
@@ -41,12 +43,22 @@ for filename in sorted(os.listdir(directory)):
             print("\tFamiliar frequency: ", familiar_count, " / ", total_count, " = ", familiar_count / total_count)
             print("\tFormal frequency: ", formal_count, " / ", total_count, " = ", formal_count / total_count)
 
+        # initialise label of the entry & append to feature list/vector
         entry_label = -1 if familiar_count > formal_count else 1
-        dataset_entry = [formal_count, familiar_count, "Dummy_Category", entry_label]
+        dataset_entry.append(formal_count)
+        dataset_entry.append(familiar_count)
+
+        # Get last line which contains the conversation type of this entry
+        f.seek(0)
+        type_tag = f.read().splitlines()[-1]
+        if type_tag not in convo_type_tags:
+            type_tag = "dummy_cat"
+        else:
+            print("\tType: ", type_tag)
+
+        dataset_entry.append(type_tag)
+        dataset_entry.append(entry_label)
         convo_dataset.append(dataset_entry)
 
 convo_dataset = np.array(convo_dataset)
-print(convo_dataset)
-
-
-
+print("\nFeature Matrix\n", convo_dataset)

f579b514ac81013bed6330063b903c323bf28e80
Ciara Gilsenan <ciaragil98@gmail.com>
2020-12-21 17:26:06 +0000

    fixed code

--- model.py
+++ model.py
@@ -6,8 +6,7 @@ from matplotlib.lines import Line2D
 from sklearn.linear_model import LogisticRegression
 from sklearn.svm import SVC
 from sklearn.model_selection import KFold, train_test_split
-from sklearn.metrics import confusion_matrix, mean_squared_error, accuracy_score, roc_curve, \
-    precision_score, recall_score, auc, classification_report
+from sklearn.metrics import confusion_matrix, mean_squared_error, accuracy_score, roc_curve, auc, classification_report
 from sklearn.dummy import DummyClassifier
 from extract_features import get_feature_matrix
 import matplotlib.pyplot as plt
@@ -15,7 +14,7 @@ import numpy as np
 import matplotlib as mpl
 # mpl.use('Qt5Agg')
 
-# Normalize Data???
+# Function to normalize formal and familiar frequencies in the feature matrix
 def normalize(X1, X2):
     """
     :param X1: Formal freq vector
@@ -35,6 +34,7 @@ def normalize(X1, X2):
     normalized_data = np.column_stack((X1, X2, X3))
     return normalized_data
 
+
 # Get training and testing data
 feature_matrix = get_feature_matrix()
 X1 = np.array(feature_matrix[:, 0:1])
@@ -47,17 +47,8 @@ y = feature_matrix[:, len(feature_matrix[0])-1:].flatten()
 x = normalize(X1, X2)
 x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, shuffle=False, stratify=None)
 
-
-# fuction to graph the data on a 3D graph
-def graph_data():
-    fig = plt.figure()
-    ax = fig.add_subplot(111, projection='3d')
-    ax.scatter(X1, X2, y)
-    plt.title("3D Graph")
-    plt.xlabel("Formal count")
-    plt.ylabel("Familiar count")
-    plt.show()
-    plt.cla()
+# initialise KFold
+kf = KFold(n_splits=5)
 
 
 def choose_gamma():
@@ -83,18 +74,45 @@ def choose_gamma():
     plt.ylabel("Mean Square Error")
     plt.errorbar(gammas, mse_vals, std_vals, ecolor='red')
     plt.show()
-    print(g, min(mse_vals))
     return g
 
+
+def choose_c_svc():
+    g = choose_gamma()
+    c_values = [0.1, 1, 10, 50, 100]
+    mse_vals = []
+    std_vals = []
+    for c in c_values:
+        tmp_errors = []
+        for train, test in kf.split(x_train):
+            model = SVC(kernel='rbf', gamma=g, C=c)
+            model.fit(x_train[train], y_train[train])
+            ypred = model.predict(x_train[test])
+            tmp_errors.append(mean_squared_error(y_train[test], ypred))
+        mse_vals.append(np.mean(tmp_errors))
+        std_vals.append(np.std(tmp_errors))
+
+    c = c_values[mse_vals.index(min(mse_vals))]
+
+    plt.cla()
+    label = "Kernelized SVM Cross-Validation For C"
+    plt.cla()
+    plt.title(label)
+    plt.xlabel("C")
+    plt.ylabel("Mean Square Error")
+    plt.errorbar(c_values, mse_vals, std_vals, ecolor='red')
+    plt.show()
+    return c
+
+
 # choose a penalty weight C for the Logistic Regression Model using k-fold cross validation
-def choose_c():
+def choose_c_log():
     mean_error = []
     std_error = []
     Ci_range = [0.1, 0.5, 1, 5, 10, 50]
     for Ci in Ci_range:
         model = LogisticRegression(penalty='l2', solver='lbfgs', C=Ci)
         temp = []
-        kf = KFold(n_splits=5)
         for train, test in kf.split(x):
             model.fit(x[train], y[train])
             ypred = model.predict(x[test])
@@ -102,15 +120,28 @@ def choose_c():
         mean_error.append(np.array(temp).mean())
         std_error.append(np.array(temp).std())
 
-    print("C mean square error: ", mean_error)
+    # print("C mean square error: ", mean_error)
+    c = Ci_range[mean_error.index(min(mean_error))]
+    # print(c)
+    plt.cla()
     plt.title('C vs Mean Square Error')
-    plt.errorbar(Ci_range, mean_error, yerr=std_error, linewidth=3)
+    plt.errorbar(Ci_range, mean_error, yerr=std_error, linewidth=3, ecolor='red')
     plt.xlabel('C')
     plt.ylabel('Mean square error')
     plt.show()
+    return c
 
-choose_c()
 
+# fuction to graph the data on a 3D graph
+def graph_data():
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+    ax.scatter(X1, X2, y, c='red')
+    plt.title("Normalized Data")
+    plt.xlabel("Formal count")
+    plt.ylabel("Familiar count")
+    plt.show()
+    plt.cla()
 
 def graph_predictions():
     plt.title('Logistic Regression Predictions')
@@ -128,8 +159,6 @@ def graph_predictions():
     plt.cla()
 
 
-plt.rc('font', size=18)
-plt.rcParams['figure.constrained_layout.use'] = True
 graph_data()
 
 # plot the data on a 2D graph
@@ -147,64 +176,56 @@ for i in range(len(y)):
         plt.scatter(X1[i], X2[i], color="blue", marker='_')
 
 
-# Cross validation for hyperparameters gamma and C for kernalised svm
-# Other CV method --- Able to create graphs
-kf = KFold(n_splits=5)
-g = choose_gamma()
-c_values = [0.1, 1, 10, 50, 100]
-mse_vals = []
-std_vals = []
-for c in c_values:
-    tmp_errors = []
-    for train, test in kf.split(x_train):
-        model = SVC(kernel='rbf', gamma=g, C=c)
-        model.fit(x_train[train], y_train[train])
-        ypred = model.predict(x_train[test])
-        tmp_errors.append(mean_squared_error(y_train[test], ypred))
-    mse_vals.append(np.mean(tmp_errors))
-    std_vals.append(np.std(tmp_errors))
-
-c = c_values[mse_vals.index(min(mse_vals))]
-print(c, min(mse_vals))
-
-plt.cla()
-label = "Kernelized SVM Cross-Validation For C"
-plt.cla()
-plt.title(label)
-plt.xlabel("C")
-plt.ylabel("Mean Square Error")
-plt.errorbar(c_values, mse_vals, std_vals, ecolor='red')
-plt.show()
-
-
-# Train Kernelised SVC with chosen gamma and C
-SVC = SVC(kernel='rbf', gamma=g, C=c)
-SVC.fit(x_train, y_train)
-predictions = SVC.predict(x_test)
-predictions = predictions.reshape(-1, 1)
-
-print("Kernel_SVC Intercept:", SVC.intercept_)
-# coef_ is only for linear kernels -- Gaussian is a curve
-
+# TRAIN MODELS
 # Train a logistic regression classifier
 logistic_model = LogisticRegression(penalty='l2', solver='lbfgs', C=5)
 logistic_model.fit(x_train, y_train)
-
+c_log = choose_c_log()
 # Predict target values with sklearn logistic model
 logistic_preds = logistic_model.predict(x_test)
 
-print("Logistic Regression Coefficients: ", logistic_model.coef_)
+print("\nLOGISTIC REGRESSION:\nLogistic Regression Coefficients: ", logistic_model.coef_)
 print("Logistic Regression Intercept", logistic_model.intercept_)
 print("Accuracy score: ", accuracy_score(y_test, logistic_preds))
+print("Logistic Regression Confusion Matrix\n", confusion_matrix(y_test, logistic_preds))
+print(classification_report(y_test, logistic_preds))
+
+
+# Train Kernelised SVC with chosen gamma and C
+g = choose_gamma()
+c = choose_c_svc()
+SVC = SVC(kernel='rbf', gamma=g, C=c)
+SVC.fit(x_train, y_train)
+svc_predictions = SVC.predict(x_test)
+svc_predictions = svc_predictions.reshape(-1, 1)
+
+# Plot Predictions & Test Data for Kernel_SVC
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+plt.title('Predictions & Test Data - Kernel_SVC')
+ax.set_xlabel('Formal')
+ax.set_ylabel('Familiar')
+ax.set_zlabel('Label')
+ax.scatter(x_test[:, 0], x_test[:, 1], svc_predictions, color='green', marker='D', label='predictions')
+ax.scatter(x_test[:, 0], x_test[:, 1], y_test, color='red', marker='+', label='test data')
+plt.legend()
+plt.show()
+
+# Evaluation
+print("\nKERNEL_SVC\n", mean_squared_error(y_test, svc_predictions))
+print("Accuracy on test data:", accuracy_score(y_test, svc_predictions))
+print("Kernel_SVC Intercept:", SVC.intercept_)
+print("Confusion Matrix:\n", confusion_matrix(y_test, svc_predictions))
+print(classification_report(y_test, svc_predictions))
+# coef_ is only for linear kernels -- Gaussian is a curve
 
 
 # create a baseline model and print its confusion matrix
 dummy = DummyClassifier(strategy="most_frequent").fit(x_train, y_train)
 ydummy = dummy.predict(x_test)
-print("Baseline Accuracy score: ", accuracy_score(y_test, ydummy))
+print("\nBaseline Accuracy score: ", accuracy_score(y_test, ydummy))
 
 # Calculate the confusion matrices for Logistic Regression and baseline model
-print("\nLogistic Regression Confusion Matrix", confusion_matrix(y_test, logistic_preds))
 print("Baseline Model Confusion Matrix", confusion_matrix(y_test, ydummy))
 
 
@@ -215,12 +236,12 @@ def plot_ROC():
     # logistic regression
     fpr, tpr, _ = roc_curve(y_test, logistic_model.decision_function(x_test))
     plt.plot(fpr, tpr, label="Logistic")
-    print("AUC:", auc(fpr, tpr))
+    print("\nAUC Logistic:", auc(fpr, tpr))
 
     # Kernel_SVC
     fpr, tpr, _ = roc_curve(y_test, SVC.decision_function(x_test))
     plt.plot(fpr, tpr, label="SVC")
-    print("AUC:", auc(fpr, tpr))
+    print("AUC Kernel_SVC:", auc(fpr, tpr))
 
     # baseline classifier
     fpr, tpr, _ = roc_curve(y_test, ydummy)
@@ -235,26 +256,3 @@ def plot_ROC():
 
 
 plot_ROC()
-
-print(classification_report(y_test, logistic_preds))
-
-
-# Evaluation
-print("\nEVALUATION OF KERNEL_SVC\nMSE on test data:", mean_squared_error(y_test, predictions))
-print("Accuracy on test data:", accuracy_score(y_test, predictions))
-print("Recall Score:", recall_score(y_test, predictions))
-print("Precision Score:", precision_score(y_test, predictions))
-print("Confusion Matrix:\n", confusion_matrix(y_test, predictions))
-
-
-# Plot Predictions & Training for Kernel_SVC
-fig = plt.figure()
-ax = fig.add_subplot(111, projection='3d')
-plt.title('3D Predictions & Test Data')
-ax.set_xlabel('Formal')
-ax.set_ylabel('Familiar')
-ax.set_zlabel('Label')
-ax.scatter(x_test[:, 0], x_test[:, 1], predictions, color='green', marker='D', label='predictions')
-ax.scatter(x_test[:, 0], x_test[:, 1], y_test, color='red', marker='+', label='test data')
-plt.legend()
-plt.show()
